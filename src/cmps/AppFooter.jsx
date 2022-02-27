ed4014f62a7c8bec4a855557b77d5933f1ec38b6
koren850 <korenah3@gmail.com>
2022-02-27 14:29:31 +0200

    Added git and linkedin to footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,4 +1,6 @@
 import logo from "../assets/imgs/logo.png";
+import git from "../assets/imgs/svg/github.svg";
+import linkdin from "../assets/imgs/svg/linkedin.svg";
 
 export function AppFooter({ isDark, setIsDark }) {
 	return (
@@ -7,6 +9,12 @@ export function AppFooter({ isDark, setIsDark }) {
 				<div>
 					<img src={logo} />
 					<span>Ping-U</span> Ⓒ 2022 Made By Koren Aharon
+					<a target='_blank' href='https://github.com/koren850'>
+						<img src={git} />
+					</a>
+					<a target='_blank' href='https://www.linkedin.com/in/koren-aharon/'>
+						<img src={linkdin} />
+					</a>
 				</div>
 			</footer>
 		</section>
